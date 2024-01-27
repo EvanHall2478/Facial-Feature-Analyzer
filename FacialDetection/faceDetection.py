59622751537b21d9900c74046db92dd7f407ee02
Dharma Gupta <dharma.anita@gmail.com>
2024-01-26 21:54:43 -0500

    Initial Commit

---
+++ FacialDetection/faceDetection.py
@@ -0,0 +1,36 @@
+import os
+import cv2 as cv
+import numpy as np
+
+def create_train(): 
+    faces_roi = None
+    people = []
+    for i in os.listdir(r'C:\Users\16134\OneDrive\Documents\Learning\Software\Courses\OpenCV\Photos'): 
+        people.append(i) 
+    print(people) 
+
+    DIR = r'C:\Users\16134\OneDrive\Documents\Learning\Software\Courses\OpenCV\Photos'
+    haar_cascade = cv.CascadeClassifier(r'haar_face.xml')
+
+    features = [] 
+    labels = []
+    
+    for person in people: 
+        path = os.path.join(DIR, person) 
+        label = people.index(person)
+        print(label)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+
+            img_array = cv.imread(img_path) 
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4) 
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w] 
+                features.append(faces_roi) 
+                labels.append(label) 
+
+    return features, labels
